597bcaaf76342370df13f12a179d9a95348db68d
Pavel.V <vaninen@ya.ru>
2023-10-30 23:17:31 +0300

    initial commit

---
+++ src/Seminar01/Main.java
@@ -0,0 +1,8 @@
+package Seminar01;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class Main {
+    public static void main(String[] args) {
+        assertThat(1+1).isEqualTo(3);
+        }
+    }
